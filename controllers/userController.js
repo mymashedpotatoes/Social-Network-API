d709dcae3d2b9548fb22a3ff196716befd2e4718
ella gaul <145066673+mymashedpotatoes@users.noreply.github.com>
2024-04-01 00:31:06 -0500

    added user routes and controller

---
+++ controllers/userController.js
@@ -0,0 +1,76 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // get all users
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // get a single user
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .select('-__v');
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // create a new user
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // update user
+    async updateUser(req, res) {
+        try {
+            const result = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+    
+            if (result) {
+                console.log(`Updated user: ${result}`);
+                res.status(200).json(result);
+            } else {
+                console.log('User not found.');
+                res.status(404).json({ message: 'User not found.' });
+            }
+        } catch (err) {
+            console.error('Error updating user:', err);
+            res.status(500).json({ message: 'Something went wrong.' });
+        }
+    },
+
+    // delete user
+    async deleteUser(req, res) {
+        try {
+          const user = await User.findOneAndDelete({ _id: req.params.userId });
+    
+          if (!user) {
+            return res.status(404).json({ message: 'No user with that ID' });
+          }
+          
+          // delete associated thoughts
+          await Thought.deleteMany({ _id: { $in: user.thoughts } });
+          res.json({ message: 'User and associated thoughts deleted!' })
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      },
+}
--- models/User.js
+++ models/User.js
@@ -16,12 +16,18 @@ const userSchema = new Schema(
             unique: true,
             match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
         },
-        thoughts: [thoughtSchema],
-        friends: [userSchema],
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }],
     },
     {
         toJSON: {
-          virtuals: true,
+            virtuals: true,
         },
         id: false,
     },
@@ -29,11 +35,11 @@ const userSchema = new Schema(
 
 // Create a virtual property `friendCount` that gets legth of user's friends array on query
 userSchema
-  .virtual('friendCount')
-  // Getter
-  .get(function () {
-    return this.friends.length;
-  });
+    .virtual('friendCount')
+    // Getter
+    .get(function () {
+        return this.friends.length;
+    });
 
 const User = model('user', userSchema);
 
